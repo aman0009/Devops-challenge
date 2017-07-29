d764af06be5ca74d821d36e756898aaa9cc3b784
hellloamman <amansharma33.as@gmail.com>
2017-07-30 01:40:43 +0530

    micro

---
+++ MsTest.UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MsTest.UnitTests
+{
+    [TestClass]
+    public class ShoppingBasketTests
+    {
+        [TestMethod]
+        public void YourFirstTestMethod()
+        {
+        }
+        [TestMethod]
+        public void YourSecondTestMethod()
+        {
+        }
+    }
+}
---
+++ Nunit.UnitTests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Nunit.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void YourFirstTestMethod()
+        {
+        }
+        [Test]
+        public void YourSecondTestMethod()
+        {
+        }
+    }
+}
